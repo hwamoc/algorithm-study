b9ba83ea293012606c4a85d03dc883823bb50966
hwamoc <hwamok222@naver.com>
2020-06-07 04:24:28 +0900

    1주차 해시 & 스택/큐 문제 풀이

---
+++ hwamoc/lv2-전화번호_목록.py
@@ -0,0 +1,12 @@
+def solution(phone_book):
+    phone_book.sort()
+
+    for i in range(1, len(phone_book)):
+        if phone_book[i].find(phone_book[0]) == 0:
+            # if phone_book[i].startswith(phone_book[0]):
+            return False
+
+    return True
+
+print(solution([6, 10, 2]))
+print(solution([3, 30, 34, 5, 9]))
